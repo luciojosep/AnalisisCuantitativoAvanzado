37dd3f9a6a2ec640124e8b11f6418b4266d08cba
Leonardo Caravaggio <46936817+LCaravaggio@users.noreply.github.com>
2023-07-05 16:09:55 -0300

    Update get-x13as.sh

--- SeriesdeTiempo/get-x13as.sh
+++ SeriesdeTiempo/get-x13as.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 X13AS_DIR=x13asall/
-X13AS_TARFILE=x13asall_V1.1_B39.tar.gz
+X13AS_TARFILE=x13as_all-v1-1-b39.tar
 
 mkdir -p $X13AS_DIR
 wget -P $X13AS_DIR -c https://www.census.gov/ts/x13as/unix/$X13AS_TARFILE
